066bead2ef181c361b454fce6700c55b1420a902
Thiago <thiagohpr@al.insper.edu.br>
2021-04-05 20:03:36 -0300

    adiciona calculadora CFC

--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -2,4 +2,17 @@ package br.edu.insper.desagil.joice.model;
 
 public class CalculadoraCFC extends Calculadora{
 
+	public CalculadoraCFC() {
+		super("Cúbica de Face Centrada");
+	}
+
+	@Override
+	public double calcula(double peso, double raio) {
+		MassaCFC m = new MassaCFC(peso);
+		VolumeCFC v = new VolumeCFC(raio);
+		
+		double densidade=m.massaTotal()/(double)v.volTotal();
+		return densidade;
+	}
+
 }

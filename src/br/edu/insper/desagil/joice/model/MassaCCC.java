ce69f5b8bc3f509b846f5ce2e7d4aaa62991e32f
PedroZequi <62962214+PedroZequi@users.noreply.github.com>
2021-04-05 20:01:41 -0300

    adiciona volume, massa e calculadora do CCC

--- src/br/edu/insper/desagil/joice/model/MassaCCC.java
+++ src/br/edu/insper/desagil/joice/model/MassaCCC.java
@@ -1,5 +1,18 @@
 package br.edu.insper.desagil.joice.model;
 
 public class MassaCCC {
-
+	private int atomos;
+	private double peso;
+	
+	public MassaCCC (double peso) {
+		this.atomos = 2;
+		this.peso = peso;
+	}
+	
+	public double massaTotal() {
+		double avogrado = 6.022*Math.pow(10, 23);
+		double	massa = (this.atomos*this.peso)/(double)avogrado;
+		return massa;
+		
+	}
 }
